59f534c4e4faa72d30b3ee19c4002a9d09e8f8f4
rahim einollahi <r.einollahi@gamil.com>
2022-04-23 15:40:18 +0430

    Top header added & Bootstrap too installed

--- src/components/VueOrg.vue
+++ src/components/VueOrg.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
+    <div class="container mt-3 ">
+        <div class="w-100 p-2 " id="vueTopHeader">
+            <span class=" "> Vue3 is now the new default version! </span>
+            <router-link class="inside-block text-white" to="/about">Learn More</router-link>
+        </div>
         <h1>this component were been simulated from vuejs.org main pages.</h1>
         <h2>This page edit from github pages....</h2>
     </div>
@@ -12,5 +16,9 @@
 </script>
 
 <style lang="scss" scoped>
+#vueTopHeader{
+    background-color: rgba(29, 236, 150, 0.63);
+    color: white;
+}
 
 </style>
